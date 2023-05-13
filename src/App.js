a542288c96252b0f187ebd33f4cae60f9d87303f
Avinash Kumar <avinash.kumar@cognizer.ai>
2023-05-13 13:58:43 +0530

    adding simple component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
+import UserList from './UserList.js';
+import AdminList from './AdminList.js';
+import ComponentList from './ComponentList.js';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={<UserList />} />
+      <Route path="/admin" element={<AdminList />} />
+      <Route path="/component" element={<ComponentList />} />
+    </Routes>
+  </Router>
   );
 }
+
 
 export default App;
---
+++ src/UserList.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import mockData from './data/users.json';
+
+
+
+ function UserList() {
+  const [users, setUsers] = useState([]);
+  const [mockUsers, setMockUsers] = useState([]);
+
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:8080/api/users/',
+      headers: { }
+    };
+
+
+  function getData(){
+   axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }
+    useEffect(() => {
+      const fetchUsers = async () => {
+        try {
+          const response = await Promise.resolve({ data: mockData });
+          setMockUsers(response.data.users);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      fetchUsers();
+    }, []);
+
+  useEffect(() => {
+    getData();
+  }, []);
+  useEffect(()=>{
+  console.info(users);
+  },[users])
+
+  return (
+  <>
+  <p>Users</p>
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+    <ul>
+          {mockUsers.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+    </>
+  );
+}
+export default UserList
